3b6032e27e390aadda6122d0d1dae0bb1e8370e3
Evan Dorn <evan@lrdesign.com>
2016-09-07 19:37:42 -0700

    add a pentagon test for polygon centroid

--- test/collidex/geometry/polygon_test.exs
+++ test/collidex/geometry/polygon_test.exs
@@ -18,6 +18,11 @@ defmodule TestPolygon do
       assert_in_delta(x, 0.0, 0.00001)
       assert_in_delta(y, 0.0, 0.00001)
     end
+    context "of a regular pentagon centered on 1,1" do
+      {x,y} = Polygon.center(Polygon.make([{1.2150,1.0}, {1.0664, 0.7955}, {0.8261, 0.8736}, {0.8261, 1.1264}, {1.0664, 1.2045}]))
+      assert_in_delta(x, 1.0, 0.00001)
+      assert_in_delta(y, 1.0, 0.00001)
+    end
   end
 
 end
